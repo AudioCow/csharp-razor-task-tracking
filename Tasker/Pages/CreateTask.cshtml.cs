ce717090cd8b1ae878867b67b3cacfb2dbd1feb2
Zach <zbhaywood@gmail.com>
2021-05-22 18:58:55 -0600

    forgot return

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -16,7 +16,7 @@ namespace Tasker.Pages
         }
         public IActionResult OnPost()
         {
-            RedirectToPage("Index");
+            return RedirectToPage("Index");
         }
     }
 }
