a772530f48828f89146662f95ee94500ff1949d4
Zach <zbhaywood@gmail.com>
2021-05-22 19:26:44 -0600

    forgot the @

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,9 +1,6 @@
-using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
@@ -11,14 +8,14 @@ namespace Tasker.Pages
     {
         public IEnumerable<Task> Tasks { get; set; }
         private ApplicationDbContext _context;
-        
+
         public IndexModel(ApplicationDbContext context)
         {
             _context = context;
         }
         public void OnGet()
         {
-        Tasks = _context.Tasks.OrderBy(x=>x.Priority);
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
